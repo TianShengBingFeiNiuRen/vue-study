dfb1657be927585f4e48c711ef0920f1167e95e2
Andon <17610835229@163.com>
2022-08-10 10:52:49 +0800

    Update

--- js/weather.js
+++ js/weather.js
@@ -2,20 +2,26 @@
 new Vue({
     el: "#app",
     data: {
-        city: "杭州"
+        city: "杭州",
+        weatherList: [],
+        hotCities: ["北京", "上海", "广州", "深圳", "杭州"]
     },
     methods: {
         searchWeather(city) {
+            var that = this;
             // TODO 天气查询
-            axios.get("http://wthrcdn.etouch.cn/weather_mini?city=" + this.city)
+            axios.get("http://wthrcdn.etouch.cn/weather_mini?city=" + city)
                 .then(function (response) {
-                    console.log(response.data.data.forecast);
-                    alert(response.data.data.forecast);
+                    // console.log(response.data.data.forecast);
+                    that.weatherList = response.data.data.forecast;
                 })
                 .catch(function (error) {
                     console.log(error);
                     alert(error);
                 })
+        }, changeCity(city) {
+            this.city = city;
+            this.searchWeather(city);
         }
     }
 })
